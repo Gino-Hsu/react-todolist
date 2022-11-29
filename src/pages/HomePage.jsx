b477826d23eb962478f90c12ef3f404af104b3c6
GinoHsu <ginohsu16@gmail.com>
2022-11-29 16:16:34 +0800

    feat: 顯示使用者&掛載logout

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,18 @@
+import { useEffect } from 'react';
+import { useAuth } from 'contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { isAuthenticate } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticate) {
+      navigate('/todos');
+    } else {
+      navigate('login');
+    }
+  }, [navigate, isAuthenticate]);
 };
 
 export default HomePage;
--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -8,7 +8,7 @@ const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
   const [todos, setTodos] = useState([]);
   const navigate = useNavigate();
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, currentMember } = useAuth();
 
   useEffect(() => {
     if (!isAuthenticated) {
@@ -159,7 +159,7 @@ const TodoPage = () => {
   return (
     <div>
       TodoPage
-      <Header />
+      <Header username={currentMember?.name} />
       <TodoInput
         inputValue={inputValue}
         onChange={handleChange}
